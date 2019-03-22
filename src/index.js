2125b4c0cd5d262b42778acdcbb33d3f532a99ce
PsqNew <balahonov.dmitry@gmail.com>
2019-03-22 12:01:56 +0300

    Add: add solution Task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-// PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+ module.exports =  function makeExchange(currency) {
+    let coinValue ={"H": 50, "Q": 25, "D": 10, "N": 5, "P": 1};
+    let count;
+    let purseCoins = {};
+    if (currency > 10000) {
+        return {
+            error: "You are rich, my friend! We don't have so much coins for exchange"
+        }
+    }
+    while (currency > 0) {
+        for (let i in coinValue){
+            count = Math.floor(currency/coinValue[i]);
+            if (count >= 1) {
+                purseCoins[i] = count;
+                currency -= count * coinValue[i];
+            }
+        }
+    }
+    return purseCoins;
+};
